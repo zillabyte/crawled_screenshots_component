a063ed27453fcd39f890b6e769e3c3462fd9703d
sjarvie <shane.jarvie@gmail.com>
2014-06-27 12:16:12 -0700

    update

--- component.rb
+++ component.rb
@@ -3,7 +3,7 @@ require 'zillabyte'
 CRAWL_DOMAIN_ID = 666
 SCREENSHOT_ID = 999
 
-comp = Zillabyte.component "stripe_web_crawl_screenshots"
+comp = Zillabyte.component "web_crawl_screenshots"
 
 # Declare the schema for inputs to the component
 input_stream = comp.inputs do
@@ -13,18 +13,45 @@ end
 
 # Call the 'crawl_domain' component; which will crawl the given incoming 'domain'
 # every tuple found with the form ("url", "html)
-crawl_stream = stream.call_component do
-  component_id CRAWL_DOMAIN_ID
-  name "crawl_domain"
+stream = stream.call_component("crawl")
+
+
+# The stream now contains HTML... save it to persistent storage... 
+stream = stream.each do |tuple|
+  
+  url = tuple[:url]
+  html = tuple[:html]
+  
+  # TODO: save html to s3 storage... 
+  log "saving html to: #{url}"
+  
+  # Emit the URL back to the stream for the screenshot.. 
+  emit :url => url
+  
 end
+
 
 # Now call the 'web_screenshot' component, which will take a screenshot
 # on the incoming 'url' field and return the tuple ('url', 'html', 'image_url'),
 # where the 'image_url' is a public facing URL of the screenshot
-screenshot_stream = stream.call_component do
-  component_id SCREENSHOT_ID
-  name "web_screenshot"
+stream = stream.call_component("web_screenshot")
+
+
+# Now, save the png images
+stream = stream.each do |tuple|
+  
+  url = tuple[:url]
+  png_b64 = tuple[:png_image_b64]
+  
+  # TODO: Save the image to a store of your choice such as S3
+  
+  # Emit the url back to the stream, just so the caller knows what we've processed... 
+  emit :url => url 
+  
 end
 
-# Output tuples
-screenshot_stream.outputs
+
+# Output the urls
+stream.outputs do
+  field :url, :string
+end
